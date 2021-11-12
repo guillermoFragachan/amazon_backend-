97b08d8024b701062f71b251848a60a180888c53
guillermoFragachan <guillefragachan@gmail.com>
2021-11-12 14:57:19 +0100

    sdadasd

--- src/services/cart/shemabook.js
+++ src/services/cart/shemabook.js
@@ -1,9 +1,18 @@
-const bookSchema = new Schema(
-    {
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+
+
+const BookModel = new Schema(
+ 
+ 
+ 
+  {
       name: { type: String, required: true },
       title: { type: String, required: true },
     },
     { timestamps: true }
   )
 
-  export default model("Book", cartSchema)
+  export default model("Book", BookModel)
---
+++ src/services/products/index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import producSchema from "./schema.js"
+
+const productsRouter = express.Router()
+
+productsRouter.post ("/", async(req, res, next)=> {
+    try{
+        const newProduct = new productModel(req.body)
+        const {_id} = await newProduct.save()
+        res.status(201).send({_id})
+    }catch (error) {
+        next(error)
+    }
+})
+
+productsRouter.get("/", async(req, res, next)=> {
+
+    try {
+        const products = await productModel.find()
+        res.send(products)
+    }catch (error) {
+        next(error)
+    }
+})
+
+
+
+productsRouter.get("/:productId", async(req, res, next)=> {
+    try {
+        const id = req.params.productId
+
+        const product = await productModel.findById(id)
+        if (product) {
+            res.send(product)
+        }
+    }catch (error) {
+        next(error)
+    }
+})
