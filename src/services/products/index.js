d4ceea5e76761b28b6b9d49f781d4c18c0589f63
clodsmr <87863094+clodsmr@users.noreply.github.com>
2021-11-12 15:31:07 +0100

    products endpoints

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -36,10 +36,55 @@ productsRouter.get("/:productId", async(req, res, next)=> {
         const product = await ProductModel.findById(id)
         if (product) {
             res.send(product)
+        }else{
+            res
+            .status(404)
+            .send({ message: `product with ${id} is not found!` })
         }
     }catch (error) {
         next(error)
     }
 })
+
+productsRouter.put("/:productId", async (req, res, next) => {
+    try {
+      const id = req.params.userId
+      const updatedProduct = await ProductModel.findByIdAndUpdate(id, req.body, { new: true })
+  
+      if (updatedProduct) {
+        res.send(updatedProduct)
+      } else {
+        res
+            .status(404)
+            .send({ message: `product with ${id} is not found!` })
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
+  
+  productsRouter.delete("/:productrId", async (req, res, next) => {
+    try {
+      const id = req.params.productId
+  
+      const deletedProduct = await ProductModel.findByIdAndDelete(id)
+      if (deletedProduct) {
+        res.status(204).send()
+      } else {
+        res
+        .status(404)
+        .send({ message: `product with ${id} is not found!` })
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
+
+ /*  productsRouter.post("/:productId/reviews", async(req,res,next)=> {
+      try {
+          const 
+      }
+  }) */
+  
 
 export default productsRouter
