c4c90d09b4b2e79968e8f3c4462eed1898e69dd1
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-12 15:42:20 +0100

    LOGIN AUTH

--- src/auth/index.js
+++ src/auth/index.js
@@ -25,4 +25,41 @@ authRouter.post("/register", async (req, res) => {
   }
 });
 
+// ******************* LOGIN ********************
+// LOGIN
+authRouter.post("/login", async (req, res) => {
+    try {
+      // find the user. if email = request (req.body.email)
+      const user = await UserModel.findOne({ email: req.body.email });
+      // if there is no user with that email send a status error
+      !user && res.status(401).json("Wrong password or username!");
+  
+      // crypto-js script to decrypt password
+      const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+      const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+      // so, basically the above code is going to decryp this, so we can compare -->
+      // if the original password is not the same as the password that we sent, send a status error
+      originalPassword !== req.body.password &&
+        res.status(401).json("Wrong password or username!");
+      // and if they equal, we can send our user
+  
+      // json web token to secure our Login
+      const accessToken = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.SECRET_KEY,
+        { expiresIn: "5d" }
+      );
+  
+      // we don't want the password to be sent so we desctructure this password inside user.
+      // .doc (document) is all the information in our document (the content inside the user object) the one we get as a response
+      // so, what we do here is, take all the doc, take this password and other information: username, email etc... But hold this password, I need just the information. so we pass only info in .json(info)
+      const { password, ...info } = user._doc;
+  
+      res.status(200).json({ ...info, accessToken });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  });
+
 export default authRouter;
